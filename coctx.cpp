18ee765817ae61acc4e1751d91d51ce4768ab3c1
princewen <princewen@tencent.com>
2019-10-21 18:59:06 +0800

    Replace coctx_swap with Mov version

--- coctx.cpp
+++ coctx.cpp
@@ -1,24 +1,25 @@
 /*
-* Tencent is pleased to support the open source community by making Libco available.
+* Tencent is pleased to support the open source community by making Libco
+available.
 
 * Copyright (C) 2014 THL A29 Limited, a Tencent company. All rights reserved.
 *
-* Licensed under the Apache License, Version 2.0 (the "License"); 
-* you may not use this file except in compliance with the License. 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
 *	http://www.apache.org/licenses/LICENSE-2.0
 *
-* Unless required by applicable law or agreed to in writing, 
-* software distributed under the License is distributed on an "AS IS" BASIS, 
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-* See the License for the specific language governing permissions and 
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
 * limitations under the License.
 */
 
 #include "coctx.h"
+#include <stdio.h>
 #include <string.h>
-
 
 #define ESP 0
 #define EIP 1
@@ -41,7 +42,6 @@
 #define R8 12
 #define R9 13
 
-
 //----- --------
 // 32 bit
 // | regs[0]: ret |
@@ -52,87 +52,81 @@
 // | regs[5]: esi |
 // | regs[6]: ebp |
 // | regs[7]: eax |  = esp
-enum
-{
-	kEIP = 0,
-	kESP = 7,
+enum {
+  kEIP = 0,
+  kEBP = 6,
+  kESP = 7,
 };
 
 //-------------
 // 64 bit
-//low | regs[0]: r15 |
+// low | regs[0]: r15 |
 //    | regs[1]: r14 |
 //    | regs[2]: r13 |
 //    | regs[3]: r12 |
 //    | regs[4]: r9  |
-//    | regs[5]: r8  | 
+//    | regs[5]: r8  |
 //    | regs[6]: rbp |
 //    | regs[7]: rdi |
 //    | regs[8]: rsi |
 //    | regs[9]: ret |  //ret func addr
 //    | regs[10]: rdx |
-//    | regs[11]: rcx | 
+//    | regs[11]: rcx |
 //    | regs[12]: rbx |
-//hig | regs[13]: rsp |
-enum
-{
-	kRDI = 7,
-	kRSI = 8,
-	kRETAddr = 9,
-	kRSP = 13,
+// hig | regs[13]: rsp |
+enum {
+  kRDI = 7,
+  kRSI = 8,
+  kRETAddr = 9,
+  kRSP = 13,
 };
 
-//64 bit
-extern "C"
-{
-	extern void coctx_swap( coctx_t *,coctx_t* ) asm("coctx_swap");
+// 64 bit
+extern "C" {
+extern void coctx_swap(coctx_t*, coctx_t*) asm("coctx_swap");
 };
 #if defined(__i386__)
-int coctx_init( coctx_t *ctx )
-{
-	memset( ctx,0,sizeof(*ctx));
-	return 0;
+int coctx_init(coctx_t* ctx) {
+  memset(ctx, 0, sizeof(*ctx));
+  return 0;
 }
-int coctx_make( coctx_t *ctx,coctx_pfn_t pfn,const void *s,const void *s1 )
-{
-	//make room for coctx_param
-	char *sp = ctx->ss_sp + ctx->ss_size - sizeof(coctx_param_t);
-	sp = (char*)((unsigned long)sp & -16L);
+int coctx_make(coctx_t* ctx, coctx_pfn_t pfn, const void* s, const void* s1) {
+  // make room for coctx_param
+  char* sp = ctx->ss_sp + ctx->ss_size - sizeof(coctx_param_t);
+  sp = (char*)((unsigned long)sp & -16L);
 
-	
-	coctx_param_t* param = (coctx_param_t*)sp ;
-	param->s1 = s;
-	param->s2 = s1;
+  coctx_param_t* param = (coctx_param_t*)sp;
+  void** ret_addr = (void**)(sp - sizeof(void*) * 2);
+  *ret_addr = (void*)pfn;
+  param->s1 = s;
+  param->s2 = s1;
 
-	memset(ctx->regs, 0, sizeof(ctx->regs));
+  memset(ctx->regs, 0, sizeof(ctx->regs));
 
-	ctx->regs[ kESP ] = (char*)(sp) - sizeof(void*);
-	ctx->regs[ kEIP ] = (char*)pfn;
-
-	return 0;
+  ctx->regs[kESP] = (char*)(sp) - sizeof(void*) * 2;
+  return 0;
 }
 #elif defined(__x86_64__)
-int coctx_make( coctx_t *ctx,coctx_pfn_t pfn,const void *s,const void *s1 )
-{
-	char *sp = ctx->ss_sp + ctx->ss_size;
-	sp = (char*) ((unsigned long)sp & -16LL  );
+int coctx_make(coctx_t* ctx, coctx_pfn_t pfn, const void* s, const void* s1) {
+  char* sp = ctx->ss_sp + ctx->ss_size;
+  sp = (char*)((unsigned long)sp & -16LL);
 
-	memset(ctx->regs, 0, sizeof(ctx->regs));
+  memset(ctx->regs, 0, sizeof(ctx->regs));
+  void** ret_addr = (void**)(sp);
+  *ret_addr = (void*)pfn;
 
-	ctx->regs[ kRSP ] = sp - 8;
+  ctx->regs[kRSP] = sp;
 
-	ctx->regs[ kRETAddr] = (char*)pfn;
+  ctx->regs[kRETAddr] = (char*)pfn;
 
-	ctx->regs[ kRDI ] = (char*)s;
-	ctx->regs[ kRSI ] = (char*)s1;
-	return 0;
+  ctx->regs[kRDI] = (char*)s;
+  ctx->regs[kRSI] = (char*)s1;
+  return 0;
 }
 
-int coctx_init( coctx_t *ctx )
-{
-	memset( ctx,0,sizeof(*ctx));
-	return 0;
+int coctx_init(coctx_t* ctx) {
+  memset(ctx, 0, sizeof(*ctx));
+  return 0;
 }
 
 #endif
-
