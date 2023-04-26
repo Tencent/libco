4552e3d0015e7a7f3711d3423b3635b18e026bb8
Abhijit Bhopatkar <bain@crave.io>
2023-04-27 00:25:37 +0530

    Just minikube start

--- run_minikube-helm.sh
+++ run_minikube-helm.sh
@@ -7,15 +7,20 @@ minikube kubectl create namespace jenkins
 # install helm
 wget https://get.helm.sh/helm-v3.9.2-linux-amd64.tar.gz
 tar -zxvf helm-v3.9.2-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin/helm
-# install jenkins chart
-helm repo add jenkinsci https://charts.jenkins.io
-helm repo update
-helm install jenkins -n jenkins jenkinsci/jenkins
+# mv linux-amd64/helm /usr/local/bin/helm
+# # install jenkins chart
+# helm repo add jenkinsci https://charts.jenkins.io
+# helm repo update
+# helm install jenkins -n jenkins jenkinsci/jenkins
 
-echo "################ deployed? checking"
-minikube kubectl -- get all -n jenkins
-echo "################ waiting 60 seconds"
-sleep 600
-echo "################ running tests"
-helm test jenkins -n jenkins
+# #minikube kubectl -- get all -n jenkins
+# echo Waiting till jenkins deployment is ready
+# while true; do
+#     status=`minikube kubectl -- get all -n jenkins | grep "statefulset.apps/jenkins" | awk ' { print $2 }' | sed -e 's/\// /' | awk '{ print $1 }'```
+#     if [ status == "1" ]
+#     then
+#         break
+#     fi
+# done
+# echo "################ running tests"
+# helm test jenkins -n jenkins
