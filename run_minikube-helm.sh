b146f61e743f15f5c483bcfa65040f365511c73e
Abhijit Bhopatkar <bain@crave.io>
2023-06-08 16:56:26 +0530

    Clean helm chart creation and deploy

--- run_minikube-helm.sh
+++ run_minikube-helm.sh
@@ -2,23 +2,6 @@
 
 
 minikube start --force
-# create dedicated namespace where Jenkins helm chart will be installed
-minikube kubectl create namespace jenkins
-# install helm
-mv linux-amd64/helm /usr/local/bin/helm
-echo "Installing jenkins chart"
-helm repo add jenkinsci https://charts.jenkins.io
-helm repo update
-helm install jenkins -n jenkins jenkinsci/jenkins
 
-#minikube kubectl -- get all -n jenkins
-echo "Waiting till jenkins deployment is ready"
-
-while true; do
-    status=`minikube kubectl -- get all -n jenkins | grep "statefulset.apps/jenkins" | awk ' { print $2 }' | sed -e 's/\// /' | awk '{ print $1 }'```
-    if [ "$status" = "1" ]; then
-        break
-    fi
-done
-echo "Running tests"
-helm test jenkins -n jenkins
+helm create new-chart
+helm install new-chart ./new-chart
