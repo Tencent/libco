381b1ebcbfd47dc47f1e5017e2fcf07d376f8691
Imran Khan <imran@accupara.com>
2023-06-09 00:20:16 +0530

    - make changes to the run script to check for correct bring up of the service.

--- run_minikube-helm.sh
+++ run_minikube-helm.sh
@@ -3,9 +3,34 @@
 
 minikube start --force
 
-helm create new-chart
-helm install new-chart ./new-chart
+pwd
+ls
+echo "======================= Running helm ==========================="
+helm install my-demo-chart demo-chart/ --values demo-chart/values.yaml
 
-helm ls
-
+echo "==================="
+echo "Nodes:"
+kubectl get nodes
+echo "Pods:"
 kubectl get pods
+echo "Services:"
+kubectl get services
+
+echo "==================="
+export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services my-demo-chart)
+export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
+echo http://$NODE_IP:$NODE_PORT
+
+echo "waiting 30 seconds for nginx to come up properly.."
+sleep 30
+echo "Running curl now..."
+curl -i http://$NODE_IP:$NODE_PORT
+ret=$?
+echo "=================================="
+if [ "$ret" != "0" ]
+then
+	echo "NGINX Connection successful."
+else
+	echo "NGINX Connection unsuccesful."
+fi
+exit $ret
